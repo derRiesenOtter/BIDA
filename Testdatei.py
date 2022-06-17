ee4045d6ada9d01e50bc9f22b8a89ca2b086caea
derRiesenOtter <104461717+derRiesenOtter@users.noreply.github.com>
2022-06-17 18:32:29 +0200

    x

--- Testdatei.py
+++ Testdatei.py
@@ -1,19 +1,49 @@
-a=3
-b=4
+import numpy as np
 
-def starter(a,b):
-    i = 5
-    tomate(a,b,i)
+seq1 = "AGCGATTCTTG"
+seq2 = "TGCCACCTG"
 
+gapcost = [1,-1,-2]
 
-def tomate(a,b,i):
-    if i > 0:
-        i -= 1
-        print(a)
-        a = tomate(a,b,i)
-        a = a + 1
-        return a
-    else: 
-        return a
+def NeedlemanWunsch(seq1,seq2,gapcost):
+    scorematrixzeilen = len(seq1)+1
+    scorematrixspalten = len(seq2)+1
+    scorematrix = np.zeros([scorematrixzeilen,scorematrixspalten])
+    for i in range(1,scorematrixzeilen):
+       scorematrix[i][0] = scorematrix[i-1][0] + gapcost[2]
+    for j in range(1,scorematrixspalten):
+       scorematrix[0][j] = scorematrix[0][j-1] + gapcost[2]
+    for i in range(1,scorematrixzeilen):
+        for j in range(1,scorematrixspalten):
+            match_mismatch = 0
+            if seq1[i-1] == seq2[j-1]:
+                match_mismatch = gapcost[0]
+            else:
+                match_mismatch = gapcost[1]
+            scorematrix[i][j] = max((scorematrix[i-1][j-1]+match_mismatch),(scorematrix[i][j-1]+gapcost[2]),(scorematrix[i-1][j]+gapcost[2]))
+    Alignment1 = ""
+    Alignment2 = ""
+    i = scorematrixzeilen-1
+    j = scorematrixspalten-1
+    while i != 0 and j != 0:
+        maximum = max((scorematrix[i-1][j-1]),(scorematrix[i][j-1]),(scorematrix[i-1][j]))
+        print(maximum)
+        if (scorematrix[i-1][j-1]) == maximum:
+            Alignment1 = seq1[i-1] + Alignment1
+            Alignment2 = seq2[j-1] + Alignment2
+            i=i-1
+            j=j-1
+        elif (scorematrix[i][j-1]) == maximum:
+            Alignment1 = "_" + Alignment1
+            Alignment2 = seq2[j-1] + Alignment2
+            j=j-1
+        elif (scorematrix[i-1][j]) == maximum:
+            Alignment1 = seq1[i-1] + Alignment1
+            Alignment2 = "_" + Alignment2
+            i=i-1
+    print(scorematrix)
+    print(Alignment1)
+    print(Alignment2)
 
-starter(a,b)
+NeedlemanWunsch(seq1,seq2,gapcost)
+
---
+++ Uebung_09/gamma.py
@@ -0,0 +1,38 @@
+class Aligner:
+    def __init__(self,score_matrix, gap_cost):
+        self.score_matrix = score_matrix
+        self.gap_cost = gap_cost
+
+    def pairwise_align(self,seq1,seq2):
+        raise NotImplementedError(
+            "This is an abstract class, which can not align",
+            seq1,"and",seq2) 
+
+class NeedlemanWunschDnaAligner(Aligner):
+    def pairwise_align(self,seq1,seq2):
+        
+        
+
+
+s1 = Aligner("ABC","DEF")
+s2 = Aligner("ABC","DEF")
+seq1= "ABC"
+seq2 = "DEF"
+s1.pairwise_align(seq1,seq2)
+
+
+
+
+#def NeedlemanWunsch(u,v,δ):
+    #S[0][0] = 0
+    #for i in range(1,len(u)+2):
+       # S[i][0] = S[i−1][0] #+ δ(ui , −)
+    #for j in range(1,len(v)+2):
+    #    S[0][j] = S[0][j−1] #+ δ(−, vj )
+   # for i in range(1,len(u)+2):
+   #     for j in range(1,len(v)+2):
+    #        S[i][j] = max()
+
+# Kostenfaktor noch implementieren
+
+#Print(max(7,5,9))
