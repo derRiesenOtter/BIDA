f93c2b40676ca7159bcdbfc1a72a722418d855a2
derRiesenOtter <104461717+derRiesenOtter@users.noreply.github.com>
2022-06-21 23:00:57 +0200

    x

--- Uebung_09/tmp5.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-score_matrix = pd.DataFrame([[1,-1,-1,-1],[-1,1,-1,-1],[-1,-1,1,-1],[-1,-1,-1,1]])
-score_matrix.columns = ["A","G","C","T"]
-score_matrix.index = ["A","G","C","T"]
-print(score_matrix["A"]["A"])
