f8017fc95f6e88eb3e6d9eb2e98bf00854515eec
derRiesenOtter <104461717+derRiesenOtter@users.noreply.github.com>
2022-06-21 18:20:41 +0200

    x

--- gamma_2.py
+++ gamma_2.py
@@ -7,8 +7,7 @@ class Aligner:
         self.gap_cost = gap_cost
 
     def pairwise_align(self,seq1,seq2):
-        raise NotImplementedError(
-            "This is an abstract class, which can not align", seq1,"and",seq2) 
+        raise NotImplementedError ("This is an abstract class, which can not align", seq1,"and",seq2)
 
 class NeedlemanWunschDnaAligner(Aligner):
     def pairwise_align(self,seq1,seq2):
@@ -28,8 +27,6 @@ class NeedlemanWunschDnaAligner(Aligner):
             backtrack = []
             if (resultmatrix[i-1][j-1])+ self.score_matrix[seq1[i-1]][seq2[j-1]] == (resultmatrix[i][j]):
                 backtrack.append(resultmatrix[i-1][j-1])
-            elif (resultmatrix[i-1][j-1])+ self.score_matrix[seq1[i-1]][seq2[j-1]]  == (resultmatrix[i][j]):
-                backtrack.append(resultmatrix[i-1][j-1])
             if(resultmatrix[i-1][j])+ self.gap_cost == (resultmatrix[i][j]):
                 backtrack.append(resultmatrix[i-1][j])
             if(resultmatrix[i][j-1])+ self.gap_cost == (resultmatrix[i][j]):
@@ -48,32 +45,27 @@ class NeedlemanWunschDnaAligner(Aligner):
                 alignedseq1 = seq1[i-1] + alignedseq1
                 alignedseq2 = "_" + alignedseq2
                 i=i-1
-        #print(resultmatrix)
-        #print(alignedseq1,alignedseq2, sep = "\n")
         alignedsequences = (alignedseq1,alignedseq2)
         return alignedsequences        
 
-score_matrix = pd.DataFrame([[1,-1,-1,-1],[-1,1,-1,-1],[-1,-1,1,-1],[-1,-1,-1,1]])
-score_matrix.columns = ["A","G","C","T"]
-score_matrix.index = ["A","G","C","T"]
-
-gap_cost = -2
-
-# seq1 = "AGCGATTCTTG"
-# seq2 = "TGCCACCTG"
-
 def test_pairwise_align():
-    #if not "pairwise_align" in globals():
-        #print("The function 'pairwise_align' that is to be tested has not been defined yet.")
-        #return
+    score_matrix = pd.DataFrame([[1,-1,-1,-1],[-1,1,-1,-1],[-1,-1,1,-1],[-1,-1,-1,1]])
+    score_matrix.columns = ["A","G","C","T"]
+    score_matrix.index = ["A","G","C","T"]
+    gap_cost = -2
     test = NeedlemanWunschDnaAligner(score_matrix,gap_cost)
     expected_result = ("AGCGATTCTTG","TGCCAC_CT_G")
     actual_result = test.pairwise_align("AGCGATTCTTG","TGCCACCTG")
     if expected_result == actual_result:
-        print("Funktion aligniert Sequenzen korrekt zu AGCGATTCTTG, TGCCAC_CT_G")
+        print("Funktion aligniert Sequenzen korrekt zu AGCGATTCTTG und TGCCAC_CT_G")
     else:
         print("Funktioniert nicht. Erwartetes Ergebnis:",expected_result,"Erhaltenes Ergebnis:",actual_result)
     testi = Aligner
-    testi.pairwise_align(0,"AGCGATTCTTG","TGCCACCTG")
+    try:
+        testi.pairwise_align(score_matrix,"AGCGATTCTTG","TGCCACCTG")
+    except NotImplementedError:
+        print('Funktion erhebt den NotImplementedError wie erwartet')
+    else:
+        print('Funktion erhebt den NotImplementedError nicht wie erwartet')
     
 test_pairwise_align()
