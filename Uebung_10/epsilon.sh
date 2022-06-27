bea8b94753f37f4a292be5f0b56da3d93b818529
derRiesenOtter <robin.ender@icloud.com>
2022-06-27 21:42:23 +0200

    x

--- Uebung_10/epsilon.sh
+++ Uebung_10/epsilon.sh
@@ -1,2 +1,6 @@
 mafft-qinsi nuclear_SSU_rRNA.fasta > MSA_nuclear_SSU_rRNA.fasta
 mafft-qinsi plastid_SSU_rRNA.fasta > MSA_plastid_SSU_rRNA.fasta
+echo "ENA|JABBYQ010000031.1" | gotree reroot outgroup -i Tree_MSA_plastid_SSU_rRNA.tree -l - > reroot_Tree_MSA_plastid_SSU_rRNA.tree
+less reroot_Tree_MSA_plastid_SSU_rRNA.tree | gotree draw text -w 150
+echo "ENA|CACVBZ020000051.1" | gotree reroot outgroup -i Tree_MSA_nuclear_SSU_rRNA.tree -l - > reroot_Tree_MSA_nuclear_SSU_rRNA.tree
+less reroot_Tree_MSA_nuclear_SSU_rRNA.tree | gotree draw text -w 150
